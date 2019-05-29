aba1d1259a43e58daf4fb07ac474d18f6d18c85e
feihb123 <feihb123@vip.qq.com>
2019-05-29 23:24:18 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# vue_demo
+> vue 入门示例
+
+- 插值表达式
+- v-cloak/v-text/v-html
---
+++ src/main/java/cn/datacharm/vue_demo/VueDemoApplication.java
@@ -0,0 +1,13 @@
+package cn.datacharm.vue_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VueDemoApplication.class, args);
+    }
+
+}
